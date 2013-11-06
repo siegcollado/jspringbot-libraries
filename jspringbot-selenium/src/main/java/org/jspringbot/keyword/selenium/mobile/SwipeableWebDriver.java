0d45cdbb086d8ed5036d7a5e54e4ae65f096ba19
scollado <scollado@gmail.com>
2013-11-06 15:45:41 +0800

    - Rebase - Base files to be worked on for appium something something

---
+++ jspringbot-selenium/src/main/java/org/jspringbot/keyword/selenium/mobile/SwipeableWebDriver.java
@@ -0,0 +1,21 @@
+package org.jspringbot.keyword.selenium;
+
+import org.openqa.selenium.interactions.HasTouchScreen;
+import org.openqa.selenium.interactions.TouchScreen;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteTouchScreen;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+
+public class SwipeableWebDriver extends RemoteWebDriver implements HasTouchScreen {
+    private RemoteTouchScreen touch;
+
+    public SwipeableWebDriver(URL remoteAddress, Capabilities desiredCapabilities) {
+        super(remoteAddress, desiredCapabilities);
+        touch = new RemoteTouchScreen(getExecuteMethod());
+    }
+
+    public TouchScreen getTouch() {
+        return touch;
+    }
+}
